2359f163c3f3fba69155ea89654147fd962a648c
oyahid18 <oyahidgithub@gmail.com>
2025-01-20 10:16:28 +0530

    Delete Programming and Logic Design/Day 1 directory

--- Programming and Logic Design/Day 1/print statement without semi-colon.cpp
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main(){
-	if(printf("Adamas University"))
-	return 0;
-}
--- Programming and Logic Design/Day 1/size of processor.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-	double *p;
-	printf("The size of processor is %zu bit",sizeof(p)*8);
-	return 0;
-}
